316f8d07824a91cb7fe78e81e5e0a52162a26c3a
draganet <3417237276@qq.com>
2022-05-15 11:10:35 +0800

    初始化页面，路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,63 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import HomeView from "@/views/HomeView.vue";
+import LoginView from "@/views/LoginView.vue";
+import RegisterView from "@/views/RegisterView.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: "/",
+    name: "home",
+    component: HomeView,
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
+    path: "/login",
+    name: "login",
+    component: LoginView,
+  },
+  {
+    path: "/register",
+    name: "register",
+    component: RegisterView,
+  },
+  {
+    path: "/edit",
+    name: "edit",
+    component: () => import("@/views/EditView.vue"),
+  },
+  {
+    path: "/create",
+    name: "create",
+    component: () => import("@/views/CreateView.vue"),
+  },
+  {
+    path: "/detail",
+    name: "detail",
+    component: () => import("@/views/DetailView.vue"),
+  },
+  {
+    path: "/my",
+    name: "my",
+    component: () => import("@/views/MyView.vue"),
+  },
+  {
+    path: "*",
+    redirect: "/",
+  },
+  // {
+  //   path: '/about',
+  //   name: 'about',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  // }
+];
 
 const router = new VueRouter({
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
---
+++ src/views/CreateView.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        创建页面
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>

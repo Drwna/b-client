d2ccacfde33524f62a43b89eb6644e165d5e40be
draganet <3417237276@qq.com>
2022-05-17 15:52:53 +0800

    完成编辑页面

--- src/views/EditView.vue
+++ src/views/EditView.vue
@@ -2,20 +2,65 @@
   <div id="edit">
     <h1>编辑文章</h1>
     <h3>文章标题</h3>
-    <el-input></el-input>
+    <el-input v-model="title"></el-input>
     <p class="msg">限30个字</p>
     <h3>内容简介</h3>
-    <el-input type="textarea" rows="3"></el-input>
+    <el-input type="textarea" v-model="description" :autosize="{ minRows: 2, maxRows: 6 }"></el-input>
     <p class="msg">限30个字</p>
     <h3>文章内容</h3>
-    <el-input type="textarea" rows="20"></el-input>
+    <el-input type="textarea" v-model="content" :autosize="{ minRows: 4, maxRows: 30 }"></el-input>
     <p class="msg">限30个字</p>
-    <el-button>确定</el-button>
+    <p>
+      <label>是否展示到首页</label>
+      <el-switch v-model="atIndex" active-color="#13ce66"></el-switch>
+    </p>
+    <el-button @click="onEdit">确定</el-button>
   </div>
 </template>
 
 <script>
-export default {};
+import blog from '@/api/blog';
+
+export default {
+  data() {
+    return {
+      atIndex: false,
+      title: '',
+      description: '',
+      content: '',
+    };
+  },
+
+  created() {
+    this.blogId = this.$route.params;
+    blog.getDetail(this.blogId).then(res => {
+      this.title = res.data.title;
+      this.description = res.data.description;
+      this.content = res.data.content;
+      this.atIndex = res.data.atIndex;
+    });
+  },
+
+  methods: {
+    onEdit() {
+      // console.log(this.blogId);
+      blog
+        .updateBlog(this.blogId, {
+          title: this.title,
+          description: this.description,
+          content: this.content,
+          atIndex: this.atIndex,
+        })
+        .then(res => {
+          this.$message({
+            message: '修改成功',
+            type: 'success',
+          });
+          this.$router.push(`/detail/${res.data.id}`);
+        });
+    }
+  },
+};
 </script>
 
 <style lang="scss" scoped>

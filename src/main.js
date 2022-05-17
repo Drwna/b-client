af71a2f46c331b25b1f2eeb8c48c14ea3e491efa
draganet <3417237276@qq.com>
2022-05-17 17:51:08 +0800

    按需引入elementui

--- src/main.js
+++ src/main.js
@@ -2,14 +2,14 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
 import friendlyDate from '@/helpers/util';
+import { Button, Message, Pagination } from 'element-ui';
 
 Vue.config.productionTip = false;
 
-Vue.use(ElementUI);
 Vue.use(friendlyDate);
+Vue.use(Button, Message);
+Vue.use(Pagination);
 
 new Vue({
   router,
